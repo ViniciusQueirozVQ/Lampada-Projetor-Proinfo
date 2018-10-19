9139f9345d2c1c5a904b6c8b647c0ec2ba48fe64
ViniciusQueiroz-VQ <viniciusqueiroz.voq@gmail.com>
2018-10-19 10:54:52 -0300

    Organização com hierarquia FHS

--- install.sh
+++ install.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
 if [ "$(id -u)" != "0" ]; then
- echo "need root"
+ echo "Execute como superusuário  'root'"
  exit
 fi
+
+mkdir -p /usr/lampada-proinfo-ctl/
+
 echo "Digite 'epson' (sem aspas) caso o seu projetor seja um 71/2013 ou se voce tiver certeza de que o fabricante da lampada é Epson:"
 echo "Ou digite 'benq' (sem aspas) caso o seu projetor seja um 42/2010 ou se voce tiver certeza de que o fabricante da lampada é a Benq:"
 echo "Digite abaixo ..."
@@ -11,12 +14,12 @@ read fabricante
 echo $fabricante >> /usr/lampada-proinfo-ctl/fabricante.txt
 echo "OK. Você possui um Projetor" $fabricante
 
-mkdir -p /usr/lampada-proinfo-ctl/
-cp *.txt /usr/lampada-proinfo-ctl/
-cp projetor-* /usr/lampada-proinfo-ctl/
-cp *.sh /usr/lampada-proinfo-ctl/
-cp *.service /etc/systemd/system/
-cp epson-projector-cmd /bin/
+cd ~
+cp Lampada-Projetor-Proinfo/usr/* /
+cp Lampada-Projetor-Proinfo/etc/* /
+cp Lampada-Projetor-Proinfo/bin/* /
+cp Lampada-Projetor-Proinfo/lib/* /
+
 echo "O projetor desligará a lâmpada em instantes..."
 
 systemctl enable projetor-on.service
