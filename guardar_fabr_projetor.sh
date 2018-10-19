07982f58318757776862077f3ed5b27d7814e229
ViniciusQueiroz-VQ <viniciusqueiroz.voq@gmail.com>
2018-10-19 10:58:48 -0300

    Remoção de Arquivos duplicados apos hieraquização

--- guardar_fabr_projetor.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "Digite 'epson' (sem aspas) caso o seu projetor seja um 71/2013 ou se voce tiver certeza de que o fabricante da lampada é Epson:"
-echo "Ou digite 'benq' (sem aspas) caso o seu projetor seja um 42/2010 ou se voce tiver certeza de que o fabricante da lampada é a Benq:"
-echo "Digite abaixo ..."
-read fabricante
-echo $fabricante >> /usr/lampada-proinfo-ctl/fabricante.txt
-echo "OK. Você possui um Projetor" $fabricante
