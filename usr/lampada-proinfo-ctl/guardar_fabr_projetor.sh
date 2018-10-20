0f60d888c8258de705099ad9bc57668a79a6a9a3
ViniciusQueiroz-VQ <viniciusqueiroz.voq@gmail.com>
2018-10-20 10:01:39 -0300

    "Melhorias na estrutura do pacote"

--- usr/lampada-proinfo-ctl/guardar_fabr_projetor.sh
+++ usr/lampada-proinfo-ctl/guardar_fabr_projetor.sh
@@ -4,5 +4,5 @@ echo "Digite 'epson' (sem aspas) caso o seu projetor seja um 71/2013 ou se voce 
 echo "Ou digite 'benq' (sem aspas) caso o seu projetor seja um 42/2010 ou se voce tiver certeza de que o fabricante da lampada é a Benq:"
 echo "Digite abaixo ..."
 read fabricante
-echo $fabricante >> /usr/lampada-proinfo-ctl/fabricante.txt
+echo $fabricante >> /lib/lampada-projetor-proinfo-libs/fabricante
 echo "OK. Você possui um Projetor" $fabricante
