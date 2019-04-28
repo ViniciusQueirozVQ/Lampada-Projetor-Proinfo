b846711e74aa04e44a85dfca607d715f3b5f821b
Vinicius <36282777+ViniciusQueiroz-VQ@users.noreply.github.com>
2019-04-27 21:29:19 -0300

    Update guardar_fabr_projetor.sh

--- usr/lampada-proinfo-ctl/guardar_fabr_projetor.sh
+++ usr/lampada-proinfo-ctl/guardar_fabr_projetor.sh
@@ -9,6 +9,6 @@ echo "Digite 'epson' (sem aspas) caso o seu projetor seja um 71/2013 ou se voce 
 echo "Ou digite 'benq' (sem aspas) caso o seu projetor seja um 42/2010 ou se voce tiver certeza de que o fabricante da lampada é a Benq:"
 echo "Digite abaixo ..."
 read fabricante
-echo $fabricante >> /lib/lampada-projetor-proinfo-libs/vendor
+echo $fabricante >> /lib/vendor
 echo "OK. Você possui um Projetor" $fabricante
 exit
