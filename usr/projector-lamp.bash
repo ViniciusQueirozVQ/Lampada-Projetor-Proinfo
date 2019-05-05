a7a29bb8ba7b2e1eff49ccc699e980d071625cf9
Vinicius <36282777+ViniciusQueiroz-VQ@users.noreply.github.com>
2019-05-04 22:15:44 -0300

    Add files via upload

---
+++ usr/projector-lamp.bash
@@ -0,0 +1,24 @@
+#!/bin/bash
+case $1 in
+   "-on") 
+#BENQ
+		echo -e "\x0D*pow=on#\x0D" > /dev/ttyS1
+		echo -e "\x0D*auto\x0D" > /dev/ttyS1
+		echo -e "\x0D*baud=9600\x0D" > /dev/ttyS1
+#EPSON		
+		/bin/epson-projector-cmd poweron
+		/bin/epson-projector-cmd sendcmd "SOURCE 1F"
+		;;
+   "-off")
+#BENQ
+		echo -e "\x0D*pow=off#\x0D" > /dev/ttyS1
+#EPSON
+		/bin/epson-projector-cmd poweroff
+        ;;
+   *) 
+   echo "Falha ao executar" >> /var/log/lamp.log
+   exit 1
+      ;;
+esac
+
+echo "`date` $1" >> /var/log/lamp.log
